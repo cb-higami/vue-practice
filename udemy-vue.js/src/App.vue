b0fbba55a3de1bfc790f466310dad93d78b7fd15
樋上　光輝 <higami@higamimitsuterunoMacBook-Pro.local>
2022-12-25 18:15:11 +0900

    test

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AAAA msg="Welcome to Your Vue.js App" test="aaaaaa"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AAAA from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AAAA
   }
 }
 </script>
--- udemy-vue.js/src/components/HelloWorld.vue
+++ udemy-vue.js/src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ test }}</h1>
+
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -34,7 +36,9 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    test: String,
+
   }
 }
 </script>
