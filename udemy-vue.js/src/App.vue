c6236d1aca689893e90622e0085de80cee37c280
樋上　光輝 <higami@higamimitsuterunoMacBook-Pro.local>
2023-01-02 17:49:37 +0900

    component init

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <AAAA msg="Welcome to Your Vue.js App" test="aaaaaa"/>
-  </div>
+  <p>good ( {{ number }})</p>
 </template>
 
 <script>
-import AAAA from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    AAAA
+  data: function() {
+    return {
+      number : 4
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
