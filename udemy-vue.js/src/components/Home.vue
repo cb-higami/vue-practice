421e5037b4775ccc9f90d0bb59089144dfc2a22d
樋上　光輝 <higami@higamimitsuterunoMacBook-Pro.local>
2023-01-05 21:11:34 +0900

    カスタムディレクティブの作成

--- udemy-vue.js/src/components/Home.vue
+++ udemy-vue.js/src/components/Home.vue
@@ -1,3 +1,22 @@
 <template>
-    <p>home</p>
+    <p v-border:solid.round.shadow="{ width: '7px' , color : 'red'}">home</p>
 </template>
+
+<script>
+//ディレクティブ ローカル登録
+export default {
+    directives: {
+        border(el, binding) {
+            el.style.borderWidth = binding.value.width
+            el.style.borderColor = binding.value.color
+            el.style.borderStyle = binding.arg
+            if(binding.modifiers.round) {
+                el.style.borderRadius = '0.7rem'
+            }
+            if(binding.modifiers.shadow) {
+                el.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.26)'
+            }
+        }
+    }
+}
+</script>
--- udemy-vue.js/src/main.js
+++ udemy-vue.js/src/main.js
@@ -6,7 +6,29 @@ Vue.config.productionTip = false
 
 // コンポーネントのグローバル登録
 Vue.component("LikeNumber", LikeNumber);
-console.log(LikeNumber)
+// console.log(LikeNumber)
+
+
+//v ディレクティブの自作 （グローバル登録）
+// el: vディレクティブの対象エレメント
+// （<p data-v-7ba5bd90="" style="border: 2px solid black;">home</p>）
+// binding: v-xxx = "'xxx'" で渡された時の値？
+
+// Vue.directive("border", function(el, binding) {
+//   console.log(el)
+//   console.log(binding)
+
+//   // el.style.border = "solid black 2px"
+//   el.style.borderWidth = binding.value.width
+//   el.style.borderColor = binding.value.color
+//   el.style.borderStyle = binding.arg
+//   if(binding.modifiers.round) {
+//     el.style.borderRadius = '0.7rem'
+//   }
+//   if(binding.modifiers.shadow) {
+//     el.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.26)'
+//   }
+// })
 
 
 new Vue({
