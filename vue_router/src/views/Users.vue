dccef0e6d57e1561bd18615417c7fcfcecb56e07
樋上　光輝 <higami@higamimitsuterunoMacBook-Pro.local>
2023-01-11 21:54:14 +0900

    storeメソッドの呼び出し方法について

--- vue_router/src/views/Users.vue
+++ vue_router/src/views/Users.vue
@@ -7,15 +7,22 @@
         <router-link :to="'/users/' + (Number(id) +1) + '/profile'">次のユーザープロフィール</router-link>
         <router-link :to="{ name : 'users-id-profile', params: { id: Number(id) +1}}">次のユーザープロフィール</router-link>
         <router-view></router-view>
-        <button @click="increment">+1</button>
-        <button @click="decrement">-1</button>
+        <button @click="increment(1)">+1</button>
+        <button @click="decrement(1)">-1</button>
+        <!-- <button @click="increment">+1</button>
+        <button @click="decrement">-1</button> -->
         <p>{{ this.$store.state.count }}</p>
         <p>{{ doubleCount }}</p>
         <p>{{ tripleCount }}</p>
+
+        <input type="text" v-model="message">
+        <p> {{ message }}</p>
     </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
     props: ['id'],
     beforeRouteEnter(to, from, next) {
@@ -37,20 +44,46 @@ export default {
         }
     },
     methods: {
-        increment() {
-            this.$store.state.count ++;
-        },
-        decrement() {
-            this.$store.state.count--;
-        }
+        // ストアで定義したメソッドを呼び出す
+        // increment() {
+        //     this.$store.commit('increment', 1)
+        // },
+        // decrement() {
+        //     this.$store.commit('decrement', 1)
+        // }
+        // 糖衣構文
+        // ...mapMutations(['increment', 'decrement'])
+        
+        // ストアのactions を呼び出す
+        // increment() {
+        //     this.$store.dispatch('increment', 1);
+        // },
+        // decrement() {
+        //     this.$store.dispatch('decrement', 1);
+        // }
+        // 糖衣構文
+        ...mapActions(['increment', 'decrement'])
     },
     computed : {
-        doubleCount() {
-            return this.$store.getters.doubleCount
-        },
-        tripleCount() {
-            return this.$store.getters.tripleCount
-        },
-    }
+        // ストアのgetters を呼び出す
+        // doubleCount() {
+        //     return this.$store.getters.doubleCount
+        // },
+        // tripleCount() {
+        //     return this.$store.getters.tripleCount
+        // },
+        // 糖衣構文
+        ...mapGetters(['doubleCount', 'tripleCount']),
+        message : {
+            get() {
+                console.log('get')
+                return this.$store.getters.message;
+            },
+            set(value) {
+                console.log('set')
+                this.$store.dispatch('updateMessage', value)
+            }
+        }
+    },
 }
 </script>
