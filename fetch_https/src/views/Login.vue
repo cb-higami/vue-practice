b36c56457be530c617b58ac6a64f1101f2772b96
樋上　光輝 <higami@higamimitsuterunoMacBook-Pro.local>
2023-01-13 09:12:51 +0900

    test

--- fetch_https/src/views/Login.vue
+++ fetch_https/src/views/Login.vue
@@ -6,12 +6,14 @@
       <br><br>
       <label for="password">パスワード</label>
       <input id="password" type="password" v-model="password">
+      <br><br>
   
       <button @click="login">送信</button>
     </div>
   </template>
   
   <script>
+import axios from '../axios-auth';
   
   export default {
     data() {
@@ -22,7 +24,22 @@
     },
     methods: {
         login() {
-            
+            axios.post(
+                "/accounts:signInWithPassword?key=AIzaSyANeWISo5x1-CZarmYStrH46ovju6LouvU",
+                {
+                    email : this.email,
+                    password : this.password,
+                    returnSecureToken: true,
+                },
+            ).then(response => {
+                console.log(response)
+                this.email = ""
+                this.password = ""
+                alert('ログインに成功しました')
+            }).catch(e => {
+                console.log(e)
+            })
+
         }
     }
   }
